1c45d22cdd419fc285e2c7d0280b96f1aa3f4c60
Andrew Tandiawan & Elkana Lumbantobing <pair+atandiawan.elkana-ltobing@hacktiv8.com>
2016-08-31 16:06:02 +0700

    Pohon Mangga

--- mango_tree_1.js
+++ mango_tree_1.js
@@ -1,23 +1,56 @@
 "use strict"
 
 class MangoTree {
-  
+
   // Initialize a new MangoTree
   constructor() {
+    this.age = Math.floor(Math.random()*10)
+    this.height = Math.floor(Math.random()*100)
+    this.fruits = Math.floor(Math.random()*40)
+    this.healthy = Math.random()
+    this.panen = Math.floor(this.fruits*0.5)
+    if (this.healthy<0.5){
+      this.healthyStatus = "Bagus"
+    } else {
+      this.healthyStatus = "Jelek"
+    }
   }
 
   // Get current states here
+  getAge(){
+    return this.age
+  }
+  getHeight(){
+    return this.height
+  }
+  getFruits(){
+    return this.fruits
+  }
+  getHealthyStatus(){
+    return this.healthyStatus
+  }
 
   // Grow the tree
   grow() {
+    this.age = this.age +1
+    if (this.age<20){
+      this.height = this.height + Math.floor(Math.random()*10)
+    } else {
+
+    }
+    this.produceMangoes()
+    this.harvest()
   }
 
   // Produce some mangoes
   produceMangoes() {
+    this.fruits = this.fruits + Math.floor(Math.random()*10)
   }
 
   // Get some fruits
   harvest() {
+    this.panen = Math.floor(this.fruits *0.5)
+    this.fruits = this.fruits - this.panen
   }
 }
 
@@ -30,3 +63,8 @@ class Mango {
 function getRandomNumber() {
 }
 
+let manggaku = new MangoTree()
+for (let i=0;i<=20;i++){
+  console.log("Year ",i," report. Umur pohon: ", manggaku.age, ". Tinggi pohon: ", manggaku.height, ". Kualitas Buah: ", manggaku.healthyStatus, "Jumlah yang berbuah: ",manggaku.fruits, "Jumlah yg di panen: ", manggaku.panen)
+  manggaku.grow()
+}
